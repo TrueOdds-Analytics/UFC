a950d41688eb83be329f1f5e35744315f54e0f43
William <williambillqinshen@gmail.com>
2024-07-13 02:54:49 -0400

    no fucking way this is real

--- data_cleaner.py
+++ data_cleaner.py
@@ -585,9 +585,9 @@ def create_specific_matchup_data(file_path, fighter_name, opponent_name, n_past_
 
 
 if __name__ == "__main__":
-    # combine_rounds_stats('data/ufc_fight_processed.csv')
-    # calculate_elo_ratings('data/combined_rounds.csv')
-    # combine_fighters_stats("data/combined_rounds.csv")
-    # create_matchup_data("data/combined_sorted_fighter_stats.csv", 3, True)
+    combine_rounds_stats('data/ufc_fight_processed.csv')
+    calculate_elo_ratings('data/combined_rounds.csv')
+    combine_fighters_stats("data/combined_rounds.csv")
+    create_matchup_data("data/combined_sorted_fighter_stats.csv", 3, True)
     split_train_val_test('data/matchup data/matchup_data_3_avg_name.csv')
     # create_specific_matchup_data("data/combined_sorted_fighter_stats.csv", "leon edwards", "Belal Muhammad", 3, True)
